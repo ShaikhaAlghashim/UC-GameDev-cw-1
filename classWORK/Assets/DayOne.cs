a2157ef74a11a10681c6100464f325e27654beb5
ShaikhaAlghashim <142631119+ShaikhaAlghashim@users.noreply.github.com>
2023-08-25 18:53:01 +0300

    done

---
+++ classWORK/Assets/DayOne.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string heroname = "Shaikha";
+        int age = 16;
+        bool ishero = true;
+        float speed = 20f;
+
+        print(" My Name is Shaikha " + heroname + " And My Age is 16 " + age );
+
+
+
+
+        int x1 = 2 + 2;
+
+        print (x1);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
